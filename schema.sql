5233687a87be3fe8c86fc5cf4e333e10acfa7855
lucianodiego <lucianodiegomail@gmail.com>
2025-07-18 22:38:36 +0200

    Delete schema.sql

--- schema.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE persone (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    cognome TEXT,
-    nome TEXT,
-    luogo_nascita TEXT,
-    data_nascita TEXT,
-    nome_padre TEXT,
-    nome_madre TEXT
-);
